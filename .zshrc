f965472926a7c54aca13f41c01250f6586ca3264
Ian Walter <public@iankwalter.com>
2018-09-07 11:55:39 -0400

    Porting over rest of fish config to zsh

--- .zshrc
+++ .zshrc
@@ -3,8 +3,8 @@ autoload -U promptinit; promptinit
 prompt pure
 
 # https://stackoverflow.com/questions/12382499/looking-for-altleftarrowkey-solution-in-zsh
-bindkey "[C" forward-word
-bindkey "[D" backward-word
+bindkey '[C' forward-word
+bindkey '[D' backward-word
 
 # Use the antigen plugin manager: https://github.com/zsh-users/antigen
 source /usr/local/share/antigen/antigen.zsh
@@ -14,3 +14,37 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 
 # Tell Antigen that we're done.
 antigen apply
+
+# Move files to trash instead of completely removing them.
+if [ `which trash` ]; then
+  alias rm=trash
+fi
+
+# Configuration for the history command.
+export HISTTIMEFORMAT='%d/%m/%y %T '
+export HISTCONTROL=ignoredups:erasedups
+
+# Set programming language paths.
+if [ -d ~/go ]; then
+  export GOPATH=~/go
+  export PATH=$GOPATH/bin:$PATH
+fi
+
+# Add NPM default bin directory to $PATH if created during installation.
+if [ -d ~/.npm-global ]; then
+  export PATH=~/.npm-global/bin:$PATH
+fi
+
+# Linux
+if [[ $(uname) == 'Linux' ]]; then
+
+  # Fix issue with VS Code deleting things.
+  export ELECTRON_TRASH='gio'
+
+  # Create pbcopy and pbpaste aliases if xclip is installed.
+  if [ `which xclip` ]; then
+    alias pbcopy='xclip -selection clipboard'
+    alias pbpaste='xclip -selection clipboard -o'
+  fi
+
+fi
--- install.sh
+++ install.sh
@@ -15,3 +15,50 @@ fi
 if ! [[ `brew ls --versions antigen` ]]; then
   brew install antigen
 fi
+
+# Install trash CLI program if NPM is installed.
+if [[ `which npm` ]] && ! [[ `which trash` ]]; then
+  npm install -g trash-cli
+fi
+
+# macOS
+if [[ $(uname) == 'Darwin' ]]; then
+
+  # Increase window resize speed for Cocoa applications.
+  defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+  # Expand save panel by default.
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+  defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+  # Automatically quit printer app once the print jobs complete.
+  defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+  # Enable the Develop menu and the Web Inspector in Safari.
+  defaults write com.apple.Safari IncludeDevelopMenu -bool true
+  defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+  defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+  # Disable swipe navigation in Chrome.
+  defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
+
+fi
+
+# Linux
+if [[ $(uname) == 'Linux' ]]; then
+
+  # Install xclip.
+  apt-get install -y xclip
+
+
+  # Change NPM default directory if necessary to avoid permission problems.
+  if [[ $(npm config get prefix) == '/usr' ]]; then
+    mkdir ~/.npm-global
+    npm config set prefix '~/.npm-global'
+  fi
+
+fi
+
+if [[ $? == 0 ]]; then
+  echo "Installed zsh configuration successfully."
+fi
